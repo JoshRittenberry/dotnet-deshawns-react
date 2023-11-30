fb86839af0fe43f69afb4d5b8a1c2cf23530ee83
Josh Rittenberry <141363044+JoshRittenberry@users.noreply.github.com>
2023-11-30 14:26:26 -0600

    list of dogs is working

---
+++ Models/Walker.cs
@@ -0,0 +1,18 @@
+public class Walker
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Name
+    {
+        get
+        {
+            string name = null;
+            if (FirstName != null && LastName != null)
+            {
+                name = FirstName + " " + LastName;
+            }
+            return name;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,87 @@
+List<City> cities = new List<City>
+{
+    new City { Id = 1, Name = "Nashville" },
+    new City { Id = 2, Name = "Franklin" },
+    new City { Id = 3, Name = "Murfreesboro" },
+    new City { Id = 4, Name = "Clarksville" },
+    new City { Id = 5, Name = "Lebanon" }
+};
+List<Dog> dogs = new List<Dog>
+{
+    new Dog { Id = 1, Name = "Buddy", WalkerId = 7, CityId = 3 },
+    new Dog { Id = 2, Name = "Bella", WalkerId = 4, CityId = 1 },
+    new Dog { Id = 3, Name = "Charlie", WalkerId = 6, CityId = 5 },
+    new Dog { Id = 4, Name = "Daisy", WalkerId = 1, CityId = 2 },
+    new Dog { Id = 5, Name = "Max", WalkerId = 9, CityId = 2 },
+    new Dog { Id = 6, Name = "Lucy", WalkerId = 5, CityId = 3 },
+    new Dog { Id = 7, Name = "Bailey", WalkerId = 1, CityId = 4 },
+    new Dog { Id = 8, Name = "Molly", WalkerId = 5, CityId = 1 },
+    new Dog { Id = 9, Name = "Coco", WalkerId = 8, CityId = 5 },
+    new Dog { Id = 10, Name = "Ruby", WalkerId = 4, CityId = 4 },
+    new Dog { Id = 11, Name = "Rocky", WalkerId = 8, CityId = 5 },
+    new Dog { Id = 12, Name = "Sadie", WalkerId = 6, CityId = 5 },
+    new Dog { Id = 13, Name = "Lucky", WalkerId = 8, CityId = 3 },
+    new Dog { Id = 14, Name = "Maggie", WalkerId = 9, CityId = 4 },
+    new Dog { Id = 15, Name = "Toby", WalkerId = 8, CityId = 5 },
+    new Dog { Id = 16, Name = "Sophie", WalkerId = 3, CityId = 3 },
+    new Dog { Id = 17, Name = "Jack", WalkerId = 6, CityId = 1 },
+    new Dog { Id = 18, Name = "Lola", WalkerId = 7, CityId = 3 },
+    new Dog { Id = 19, Name = "Teddy", WalkerId = 8, CityId = 2 },
+    new Dog { Id = 20, Name = "Chloe", WalkerId = 4, CityId = 1 },
+    new Dog { Id = 21, Name = "Duke", WalkerId = 6, CityId = 2 },
+    new Dog { Id = 22, Name = "Zoe", WalkerId = 3, CityId = 5 },
+    new Dog { Id = 23, Name = "Bear", WalkerId = 7, CityId = 1 },
+    new Dog { Id = 24, Name = "Lily", WalkerId = 8, CityId = 3 },
+    new Dog { Id = 25, Name = "Oliver", WalkerId = 7, CityId = 1 },
+    new Dog { Id = 26, Name = "Rosie", WalkerId = 4, CityId = 3 },
+    new Dog { Id = 27, Name = "Riley", WalkerId = 6, CityId = 3 },
+    new Dog { Id = 28, Name = "Stella", WalkerId = 8, CityId = 1 },
+    new Dog { Id = 29, Name = "Buster", WalkerId = 2, CityId = 5 },
+    new Dog { Id = 30, Name = "Penny", WalkerId = 6, CityId = 3 },
+    new Dog { Id = 31, Name = "Murphy", WalkerId = 5, CityId = 1 },
+    new Dog { Id = 32, Name = "Roxy", WalkerId = 4, CityId = 2 },
+    new Dog { Id = 33, Name = "Louie", WalkerId = 7, CityId = 2 },
+    new Dog { Id = 34, Name = "Luna", WalkerId = 7, CityId = 3 }
+};
+List<Walker> walkers = new List<Walker>
+{
+    new Walker { Id = 1, FirstName = "John", LastName = "Doe" },
+    new Walker { Id = 2, FirstName = "Jane", LastName = "Doe" },
+    new Walker { Id = 3, FirstName = "Alice", LastName = "Smith" },
+    new Walker { Id = 4, FirstName = "Bob", LastName = "Johnson" },
+    new Walker { Id = 5, FirstName = "Carol", LastName = "Williams" },
+    new Walker { Id = 6, FirstName = "David", LastName = "Brown" },
+    new Walker { Id = 7, FirstName = "Eva", LastName = "Davis" },
+    new Walker { Id = 8, FirstName = "Frank", LastName = "Miller" },
+    new Walker { Id = 9, FirstName = "Grace", LastName = "Wilson" }
+};
+List<WalkerCity> walkerCities = new List<WalkerCity>
+{
+    new WalkerCity { Id = 1, CityId = 1, WalkerId = 1 },
+    new WalkerCity { Id = 2, CityId = 4, WalkerId = 1 },
+    new WalkerCity { Id = 3, CityId = 5, WalkerId = 1 },
+    new WalkerCity { Id = 4, CityId = 2, WalkerId = 1 },
+    new WalkerCity { Id = 5, CityId = 5, WalkerId = 2 },
+    new WalkerCity { Id = 6, CityId = 1, WalkerId = 3 },
+    new WalkerCity { Id = 7, CityId = 2, WalkerId = 3 },
+    new WalkerCity { Id = 8, CityId = 3, WalkerId = 3 },
+    new WalkerCity { Id = 9, CityId = 4, WalkerId = 3 },
+    new WalkerCity { Id = 10, CityId = 5, WalkerId = 4 },
+    new WalkerCity { Id = 11, CityId = 1, WalkerId = 4 },
+    new WalkerCity { Id = 12, CityId = 3, WalkerId = 4 },
+    new WalkerCity { Id = 13, CityId = 1, WalkerId = 5 },
+    new WalkerCity { Id = 14, CityId = 5, WalkerId = 6 },
+    new WalkerCity { Id = 15, CityId = 2, WalkerId = 6 },
+    new WalkerCity { Id = 16, CityId = 3, WalkerId = 6 },
+    new WalkerCity { Id = 17, CityId = 1, WalkerId = 7 },
+    new WalkerCity { Id = 18, CityId = 3, WalkerId = 7 },
+    new WalkerCity { Id = 19, CityId = 4, WalkerId = 8 },
+    new WalkerCity { Id = 20, CityId = 1, WalkerId = 8 },
+    new WalkerCity { Id = 21, CityId = 3, WalkerId = 8 },
+    new WalkerCity { Id = 22, CityId = 5, WalkerId = 9 },
+    new WalkerCity { Id = 23, CityId = 1, WalkerId = 9 }
+};
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,10 +100,39 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// Get Methods
 app.MapGet("/api/hello", () =>
 {
     return new { Message = "Welcome to DeShawn's Dog Walking" };
 });
 
+app.MapGet("/api/dogs", () =>
+{
+    return dogs.Select(d =>
+    {
+        var walker = walkers.FirstOrDefault(w => w.Id == d.WalkerId);
+        var city = cities.FirstOrDefault(c => c.Id == d.Id);
+
+        return new DogDTO
+        {
+            Id = d.Id,
+            Name = d.Name,
+            WalkerId = d.WalkerId,
+            Walker = walker != null ? new WalkerDTO
+            {
+                Id = walker.Id,
+                FirstName = walker.FirstName,
+                LastName = walker.LastName,
+            } : null,
+            CityId = d.CityId,
+            City = city != null ? new CityDTO
+            {
+                Id = city.Id,
+                Name = city.Name
+            } : null,
+            PictureURL = d.PictureURL
+        };
+    });
+});
 
 app.Run();
