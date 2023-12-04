31b3dc08056efe579e68eeddb9a920cf35aff6fa
Josh Rittenberry <141363044+JoshRittenberry@users.noreply.github.com>
2023-12-04 13:32:11 -0600

    feature/10/delete-dog

--- client/src/components/Home/DogsList.js
+++ client/src/components/Home/DogsList.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { getAllDogs } from "../../services/dogService"
+import { deleteDog, getAllDogs } from "../../services/dogService"
 import DogDetailsPopUp from "./DogDetailsPopUp"
 import "./DogsList.css"
 
@@ -13,15 +13,19 @@ export const DogsList = ({ dogs, setDogs, getAndSetDogs }) => {
         <div className="dogs-list">
             {dogs.map(dog => {
                 return (
-                    <div className="dog-container" id={dog.id} key={dog.Id} value={dog.id} onClick={() => {
-                        setSelectedDog(dog)
-                        toggle()
-                    }}>
+                    <div className="dog-container" id={dog.id} key={dog.Id} value={dog.id}>
                         <header className="dog-container-header">
-                            <i className="fa-solid fa-trash-can dog-delete"></i>
+                            <i className="fa-solid fa-trash-can dog-delete" id={dog.id} onClick={event => {
+                                deleteDog(parseInt(event.target.id)).then(() => {
+                                    getAndSetDogs()
+                                })
+                            }}></i>
                             <h6 className="dog-name">{dog.name}</h6>
                         </header>
-                        <img src={dog.pictureURL} alt="Dog Image" className="dog-image" />
+                        <img src={dog.pictureURL} alt="Dog Image" className="dog-image" onClick={() => {
+                            setSelectedDog(dog)
+                            toggle()
+                        }}/>
                     </div>
                 )
             })}
